e76857b38a26bc72dfe3fbf4b6402a7756d7b64e
ngDuyThong <thongduy0512@gmail.com>
2025-06-10 22:12:20 +0700

    test commit, push

--- src/db/helper.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = {
-  dropDB: async () => mongoose.connection.dropDatabase(),
-  closeDB: async () => mongoose.connection.close(),
-  deleteAllCollection: async () =>
-    Promise.all(Object.values(mongoose.connection.collections).map(async (collection) => collection.deleteMany({}))),
-};
--- src/db/index.js
+++
@@ -1,4 +0,0 @@
-const { connectDb } = require('./db');
-const helpers = require('./helper');
-
-module.exports = { connectDb, ...helpers };
